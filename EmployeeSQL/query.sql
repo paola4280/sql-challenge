927ff90312dd6435af0982087adf6250df0bd9a3
Paola Londono <pao4280@hotmail.com>
2024-10-22 15:13:44 -0400

    adding work

---
+++ EmployeeSQL/query.sql
@@ -0,0 +1,42 @@
+﻿SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+SELECT dept_manager.emp_no, dept_manager.dept_no, departments.dept_name, 
+		employees.first_name, employees.last_name
+FROM dept_manager
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, 
+		dept_emp.dept_no, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+SELECT employees.emp_no, employees.last_name, employees.first_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, 
+		departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name IN ('Sales', 'Development');
+
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
